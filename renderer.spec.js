ae8e3e6e24f1433b81f8a59a607de9fa5648b4d5
LvtLvt <44126697+LvtLvt@users.noreply.github.com>
2021-11-07 17:09:59 +0900

    Add: functional component renderer added

--- renderer.spec.js
+++ renderer.spec.js
@@ -1,6 +1,34 @@
+const createStyle = (style) => {
+  let styleStr = ``;
+  for (const [key, value] of Object.entries(style)) {
+    styleStr += `${key}: ${value};`;
+  }
+  return styleStr;
+}
 
-const createElement = (tag, props, children) => {
-  return `<div style="background-color: red;">hello children</div>`;
+const createElement = (tagOrComponent, props, children) => {
+
+  let $elem;
+  if (typeof tagOrComponent === 'string') {
+    $elem = document.createElement(tagOrComponent);
+  } else {
+    $elem = tagOrComponent(props);
+  }
+
+  for (const [key, value] of Object.entries(props)) {
+    switch (key) {
+      case "style": {
+        $elem.setAttribute('style', createStyle(value));
+       break;
+      }
+    }
+  }
+
+  if (children) {
+    $elem.append(children);
+  }
+
+  return $elem;
 };
 
 const React = {
@@ -8,20 +36,62 @@ const React = {
 }
 
 describe('react renderer', () => {
-  it ('should render html element', () => {
+  it ('should render html element with child which is plain text', () => {
 
     const childrenText = 'hello children';
 
     const html = React.createElement('div', {
       style: {
-        backgroundColor: 'red',
+        ['background-color']: 'red',
       },
     },
-      React.createElement('div', { }, childrenText)
+      childrenText
     );
 
-    expect(html).toEqual(`<div style="background-color: red;">${childrenText}</div>`);
+    expect(html.outerHTML).toEqual(`<div style="background-color: red;">${childrenText}</div>`);
+  });
+
+  it ('should render html element with child which is html element', () => {
+    const children = `<div>i'm child</div>`; // TODO: replace with variable
+    const html = React.createElement('div',
+      {},
+      React.createElement('div', {}, `i'm child`)
+    )
+
+    expect(html.outerHTML).toEqual(`<div>${children}</div>`);
+  });
+
+  it ('should render with functional component', () => {
+
+    function MyComponent() {
+      return React.createElement('div', {}, `i'm a boy`);
+    }
+
+    const html = React.createElement(MyComponent, {});
+
+    expect(html.outerHTML).toEqual(`<div>i'm a boy</div>`);
+  });
+});
+
+describe('react renderer util', () => {
+  it('should create style string', () => {
+    const str = createStyle({
+      ['background-color']: 'red',
+    });
+
+    expect(str).toEqual('background-color: red;');
   });
 });
 
 
+describe('html dom api', function () {
+  it('should embed style element', () => {
+    const $elem = document.createElement('div');
+
+    $elem.setAttribute('style', 'background-color: red;');
+
+    const appliedStyleString = $elem.getAttribute('style');
+    expect(appliedStyleString).toEqual('background-color: red;');
+    expect($elem.outerHTML).toEqual('<div style="background-color: red;"></div>');
+  });
+});
